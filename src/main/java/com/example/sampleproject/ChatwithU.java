1f526e0708daf7e4bde8b6fac440ca53efb4ae7e
Natnael Getachew <natnaeln4dgmail.com>
2023-01-11 18:52:35 +0300

    new

--- src/main/java/com/example/sampleproject/ChatwithU.java
+++ src/main/java/com/example/sampleproject/ChatwithU.java
@@ -1,2 +1,31 @@
-package com.example.sampleproject;public class ChatwithU {
+package com.example.sampleproject;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatwithU {
+
+    public static void main(String[] args) throws IOException {
+
+        try{
+            Socket s=new Socket("localhost",3304);
+            DataInputStream din=new DataInputStream(s.getInputStream());
+            DataOutputStream dout=new DataOutputStream(s.getOutputStream());
+            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+
+            String str="",str2="";
+            while(!str.equals("stop")){
+                str=br.readLine();
+                dout.writeUTF(str);
+                dout.flush();
+                str2=din.readUTF();
+                System.out.println("Server says: "+str2);
+            }
+
+            dout.close();
+            s.close();
+        }catch(Exception e){System.out.println(e);}
+
+    }
 }
